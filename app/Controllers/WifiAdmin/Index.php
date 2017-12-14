cb478a8eed3a22fdc1ebeb9139b4558f6f9e9994
ly2513 <626375290@qq.com>
2017-12-14 23:11:01 +0800

    更新登录、新闻等模块

--- app/Controllers/WifiAdmin/Index.php
+++ app/Controllers/WifiAdmin/Index.php
@@ -9,115 +9,110 @@
 namespace App\Controllers\WifiAdmin;
 
 use App\Controllers\BaseAdmin;
+use WifiAdmin\AdminModel;
+use WifiAdmin\AuthListModel;
 
 /**
  * Class IndexAction
  *
  * @package App\Controllers\WifiAdmin
  */
-class IndexAction extends BaseAdmin {
+class IndexAction extends BaseAdmin
+{
 
-    public function index() {
-        $nav ['m'] = $this->getActionName ();
-        $nav ['a'] = 'index';
-        $this->assign ( 'nownav', $nav );
-        // print_r($nav);
-        $L = M ( 'Authlist' );
-        $rs = $L->where()->select();
-        $this->assign('authlist_count',count($rs));
-        $this->display ();
-    }
-    public function pwd() {
-        $nav ['m'] = $this->getActionName ();
-        $nav ['a'] = 'index';
+    /**
+     * 首页
+     */
+    public function index()
+    {
+        $num =  AuthListModel::select('id')->count();
+        $this->assign('m', $this->controller );
+        $this->assign('m', $this->method );
+        $this->assign('auth_list_count', $num);
 
-        $this->assign ( 'nownav', $nav );
-        if (isset ( $_POST ) && !empty($_POST) ){
-            $pwd = I ( 'post.password' );
-            if ($pwd == "") {
-                $data ['error'] = 1;
-                $data ['msg'] = "新密码不能为空";
-                return $this->ajaxReturn ( $data );
-            }
-            if (! validate_pwd ( $pwd )) {
-                $data ['error'] = 1;
-                $data ['msg'] = "密码由4-20个字符 ，数字，字母或下划线组成";
-                return $this->ajaxReturn ( $data );
-            }
-            // 实例化一个admin模型
-            $db = D ( 'Admin' );
-            $info = $db->where ( array ('id' => $this->userid ) )->field ( 'id,user,password' )->find ();
-            log::write ( $info ['password'] );
-            if (md5 ( $_POST ['oldpwd'] ) != $info ['password']) {
-
-                $data ['error'] = 1;
-                $data ['msg'] = "旧密码不正确";
-                return $this->ajaxReturn ( $data );
-            }
-
-            $_POST ['update_time'] = time ();
-            $_POST ['password'] = md5 ( $_POST ['password'] );
-            $where ['id'] = $this->userid;
-            if ($db->where ( $where )->save ( $_POST )) {
-                $data ['error'] = 0;
-                $data ['msg'] = "更新成功";
-                return $this->ajaxReturn ( $data );
-            } else {
-                $data ['error'] = 1;
-                $data ['msg'] = $db->getError ();
-                return $this->ajaxReturn ( $data );
-            }
-        } else {
-            $this->display ();
-        }
-    }
-    public function liences() {
-        $liences = '';
-        $L = M ( 'Liences' );
-        $rs = $L->where ()->find ();
-        if (! empty ( $rs )) {
-            $liences = $rs ['liences'];
-        }
-        //print_r($_POST);exit;
-        if(isset($_POST) && !empty($_POST) && empty($liences)){
-            if($L->create()){
-                $insertid=$L->add();
-                $data ['error'] = 0;
-                $data ['msg'] = "增加授权码成功";
-                return $this->ajaxReturn ( $data );
-            }else{
-                $insertid=$L->add();
-                $data ['error'] = 1;
-                $data ['msg'] = "增加授权码失败";
-                return $this->ajaxReturn ( $data );
-            }
-        }else{
-            $this->assign('liences',$liences);
-        }
         $this->display();
     }
+
+    /**
+     * @return mixed
+     */
+    public function pwd()
+    {
+        $post = $this->request->getPost();
+        if ($post) {
+            !$post['password'] ? call_back(2, '', '新密码不能为空') : '';
+
+            if (!validate_pwd($post['password'])) {
+                call_back(2, '', '密码由4-20个字符 ，数字，字母或下划线组成');
+            }
+            $info = AdminModel::select(['id','user','password'])->whereId($this->userId)->get()->toArray();
+            $info = $info ? $info[0] : [];
+            if (md5($post['old_pwd']) != $info['password']) {
+                call_back(2, '', '旧密码不正确');
+            }
+            $post['update_time'] = time();
+            $post['password']    = md5($post['password']);
+            $status = AdminModel::whereId($this->userId)->update($post);
+            $status ? call_back(0) : call_back(2, '', '操作失败!');
+        } else {
+            $this->assign('m', $this->controller);
+            $this->assign('a', $this->method);
+            $this->display();
+        }
+    }
+
+    /**
+     * @return mixed
+     */
+//    public function lience()
+    //    {
+    //        $liences = '';
+    //        $L       = M('Liences');
+    //        $rs      = $L->where()->find();
+    //        if (!empty ($rs)) {
+    //            $liences = $rs ['liences'];
+    //        }
+    //        if (isset($_POST) && !empty($_POST) && empty($liences)) {
+    //            if ($L->create()) {
+    //                $insertid       = $L->add();
+    //                $data ['error'] = 0;
+    //                $data ['msg']   = "增加授权码成功";
+    //
+    //                return $this->ajaxReturn($data);
+    //            } else {
+    //                $insertid       = $L->add();
+    //                $data ['error'] = 1;
+    //                $data ['msg']   = "增加授权码失败";
+    //
+    //                return $this->ajaxReturn($data);
+    //            }
+    //        } else {
+    //            $this->assign('liences', $liences);
+    //        }
+    //        $this->display();
+    //    }
+
     /**
      * 数据
      * Enter description here ...
      */
-    public function systemdata() {
-        $L = M ( 'Authlist' );
-        $rs = $L->where()->select();
-        $this->assign('authlist_count',count($rs));
+    public function systemData()
+    {
+       $num =  AuthListModel::select('id')->count();
+        $this->assign('auth_list_count', $num);
         $this->display();
     }
+
     /**
      * 删除三天前数据
-     * Enter description here ...
+     *
+     * @return mixed
      */
-    public function delete_auths() {
-        $oldday = strtotime("-2 day");
+    public function delete_auths()
+    {
+        $oldDay = strtotime('-2 day');
         // 删除授权
-        $db = D ( 'AuthlistModel' );
-        $sql2 = "DELETE FROM `wifi_authlist` WHERE `last_time`< $oldday";//清除所有这台机器的授权
-        $db->query ( $sql2 );
-        $data ['error'] = 0;
-        $data ['msg'] = "清理成功";
-        return $this->ajaxReturn ( $data );
+        $status = AuthListModel::where('last_time', '<', $oldDay)->update(['is_delete'=>1]);
+        $status ? call_back(0) : call_back(2, '', '清理成功!');
     }
 }
--- app/Controllers/WifiAdmin/Login.php
+++ app/Controllers/WifiAdmin/Login.php
@@ -9,6 +9,8 @@
 namespace App\Controllers\WifiAdmin;
 
 use App\Controllers\BaseAdmin;
+use WifiAdmin\AdminModel;
+use  WifiRbac;
 
 /**
  * 管理员登入管理
@@ -16,93 +18,59 @@ use App\Controllers\BaseAdmin;
  *
  * @package App\Controllers\WifiAdmin
  */
-class LoginAction extends BaseAdmin{
-    public function index(){
+class LoginAction extends BaseAdmin
+{
+
+    /**
+     * 登录界面
+     */
+    public function index()
+    {
         $this->display();
     }
-    public function verify(){
-        import("ORG.Util.Image");
-        Image::buildImageVerify();
-    }
 
-    public function dologin(){
-        import("@.ORG.WIFIRBAC");
-        import("ORG.Util.Net.IpLocation");
-
-        $username = $this->_post('user');
-        $password =  $this->_post('password','md5');
-        if(empty($username)||empty($password)){
-            $data['error']=1;
-            $data['msg']="请输入帐号密码";
-            $this->ajaxReturn($data);
-
+    /**
+     * 登录操作
+     */
+    public function doLogin()
+    {
+        $post     = $this->request->getPost();
+        $username = $post['user_name'];
+        $password = md5($post['password']);
+        if (!$username || !$password) {
+            call_back(2, '', '请输入帐号密码1');
         }
-        /*
-        $code=$this->_post('vcode','intval,md5',0);
-
-        if($code != $_SESSION['verify']){
-            $data['error']=1;
-            $data['msg']="验证码错误";
-            $this->ajaxReturn($data);
-
-        }
-        */
-        //生成认证条件
-        $map            =   array();
         // 支持使用绑定帐号登录
-        $map['user'] = $username;
-        $map['state'] = 1;
-        $authInfo = WIFIRBAC::authenticate($map);
+        $authInfo = WifiRbac::authenticate(['user' => $username, 'state' => 1]);
+        !$authInfo ? call_back(2, '', '帐号不存在或者被禁用') : '';
         // 验证管理员登入信息
-        if($authInfo){
-
-            if($authInfo['password']!=$password){
-                $data['error']=1;
-                $data['msg']='帐号密码不正确';
-                $this->ajaxReturn($data);
-            }
-            session(C('USER_AUTH_KEY'),$authInfo['id']);
-            session('adminid',$authInfo['id']);  //用户ID
-            session('adminmame',$authInfo['user']);   //用户名
-            session('roleid',$authInfo['role']);    //角色ID
-            if($authInfo['user']==C('SPECIAL_USER')) {
-                session(C('ADMIN_AUTH_KEY'), true);
-            }
-            $User	=	M(C('USER_AUTH_MODEL'));
-            $ip		=	get_client_ip();
-            $data = array();
-
-
-            $data['last_logintime']=time();
-            $data['last_loginip']	=get_client_ip();
-            $User->where(array('id'=>$authInfo['id']))->save($data);
-
-
-            $databack['error']=0;
-            $databack['url']=U('Index/index');
-
-            $this->ajaxReturn($databack);
-
-        }else{
-            $data['error']=1;
-            $data['msg']='帐号不存在或者被禁用';
-            $this->ajaxReturn($data);
-
+        if ($authInfo['password'] != $password) {
+            call_back(2, '', '帐号密码不正确');
         }
+        $_SESSION['auth_id']    = $authInfo['id'];
+        $_SESSION['admin_id']   = $authInfo['id'];// 用户ID
+        $_SESSION['admin_mame'] = $authInfo['user'];// 用户名
+        $_SESSION['role_id']    = $authInfo['role'];// 角色ID
+        if ($authInfo['user'] == 'admin') {
+            $_SESSION['wifi_admin'] = true;
+        }
+        $data['last_login_time'] = time();
+        $data['last_login_ip']   = get_client_ip();
+        $data['update_time']     = time();
+        $status                  = AdminModel::whereId($authInfo['id'])->update($data);
+        $status ? call_back(0) : call_back(2, '', '登录失败!');
+
     }
-    // 退出
-    public function loginout(){
-        session(null);
+
+    /**
+     * 退出
+     */
+    public function loginOut()
+    {
+        $_SESSION = null;
         session_destroy();
         unset($_SESSION);
-        if(session('?'.C('USER_AUTH_KEY'))) {
-            session(C('USER_AUTH_KEY'),null);
-            redirect(U('Login/index'));
-        }else {
-            $this->error('已经退出！',U('Login/index','',true,true,true));
-        }
+        call_back(0);
     }
-
-
 
 }
