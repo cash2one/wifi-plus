63b2427f5a0be751d31f01407def4f822c0d64ca
ly2513 <626375290@qq.com>
2017-12-15 00:25:30 +0800

    更新代理等级

--- app/Models/ShopModel.php
+++ app/Models/ShopModel.php
@@ -16,4 +16,14 @@ use YP\Core\YP_Model;
 class ShopModel extends YP_Model
 {
     protected $table = 'wifi_shop';
+
+    /**
+     * 获得代理信息
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function getAgent()
+    {
+        return $this->hasOne('\Agent\AgentModel', 'id', 'pid');
+    }
 }
